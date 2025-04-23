53390869a4b489a48ad5722bdd3d922e09771bd0
dharshib <Dharshib.8@gmail.com>
2025-04-23 18:45:57 +0530

    Update navigation after successful login to include query parameters

--- client/src/services/AuthService.jsx
+++ client/src/services/AuthService.jsx
@@ -23,7 +23,7 @@ export function AuthService() {
                 }
                 dispatch(loginSuccess(userData))
                 toast.success("Login successful!");
-                navigate(`/`);
+                navigate(`/?type=today&category=`);
             }
         } catch (error) {
             processError(error);
@@ -44,7 +44,7 @@ export function AuthService() {
                 }
                 dispatch(loginSuccess(userData))
                 toast.success("Login successful!");
-                navigate(`/`);
+                navigate(`/?type=today&category=`);
             }
         } catch (error) {
             processError(error)
@@ -66,7 +66,7 @@ export function AuthService() {
                 }
                 dispatch(loginSuccess(userData))
                 toast.success("Login successful!");
-                navigate(`/`);
+                navigate(`/?type=today&category=`);
             }
         } catch (error) {
             processError(error)
