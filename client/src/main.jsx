c02019745db798959987000cad2cde99ae4d17b7
dharshib <Dharshib.8@gmail.com>
2025-04-29 09:03:56 +0530

    Add environ var config for client deployment in k8s

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={window.__RUNTIME_CONFIG__?.VITE_GOOGLE_CLIENT_ID || import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <App />
       <Toaster />
     </GoogleOAuthProvider>
